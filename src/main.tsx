fba827db55a3f40c00aefa6277f87ef54386e185
z737 <mail.abhijithms@gmail.com>
2026-04-11 14:35:28 +0530

    Switch to HashRouter for GitHub Pages compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter as BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from './context/ThemeContext'
 import App from './App'
 import './styles/globals.css'
